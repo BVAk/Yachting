43f4569e80bd7e01fe4be4aa4b31d29566996b31
Bekk Viktoria <bekkviktoria55@gmail.com>
2018-03-12 12:34:29 +0200

    Database changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    {{ Auth::user()->name }}, You are logged in! :)
                 </div>
             </div>
         </div>
